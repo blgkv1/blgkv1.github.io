e8631362f4269f3fedf0dc4c27c37c273c82e990
blgkv1 <57865893+blgkv1@users.noreply.github.com>
2025-05-15 05:33:12 +0300

    Added products filter for portfolio page

--- js/filter.js
+++ js/filter.js
@@ -1,1 +1,18 @@
-const 
+const filterButtons = document.querySelector('.button-portfolio-group')
+const items = document.querySelectorAll('.work-item')
+
+filterButtons.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'BUTTON') return;
+  
+    const filter = event.target.textContent.trim();
+  
+    items.forEach(item => {
+      const category = item.querySelector('.sample-spec').textContent.trim();
+  
+      if (filter === 'All' || category === filter) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  });
